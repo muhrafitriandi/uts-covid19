7106822733e342e3388c1d102a248568e89c698f
muhrafitriandi <61121146+muhrafitriandi@users.noreply.github.com>
2021-03-27 21:11:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 <?php include_once("index.html");
+<?php include_once("datadanstatistik.html");
+<?php include_once("kontak.html");
+<?php include_once("kuis.html");
+<?php include_once("kuis.html");
